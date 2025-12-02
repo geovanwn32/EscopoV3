226fe425287bd640b099120e575fbad0c0dc1070
Your Name <geovanisilvadeoliveira447@gmail.com>
2025-12-02 03:26:10 +0000

    Try fixing this error: `Runtime Error: Error: DropdownMenuLabel is not d

--- src/components/layout/sidebar-nav.tsx
+++ src/components/layout/sidebar-nav.tsx
@@ -34,7 +34,7 @@ import { Button } from '../ui/button';
 import { Separator } from '../ui/separator';
 import { useCompany } from '@/hooks/use-company';
 import { Avatar, AvatarFallback } from '../ui/avatar';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from '../ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuLabel } from '../ui/dropdown-menu';
 
 interface NavItem {
   href: string;
