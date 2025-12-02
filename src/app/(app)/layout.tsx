56917a22c959c47c91c4bf0a4e897b8aa115448b
Your Name <geovanisilvadeoliveira447@gmail.com>
2025-12-02 00:31:52 +0000

    Outra versão de barra (_for element <Sidebar>_)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,14 +5,14 @@ import { Sidebar, SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <Sidebar collapsible="icon" className="bg-sidebar text-sidebar-foreground">
+      <Sidebar collapsible="icon" variant="inset" className="bg-sidebar text-sidebar-foreground">
         <SidebarNav />
       </Sidebar>
       <SidebarInset>
         <Header />
-        <div className="min-h-[calc(100vh-4rem)] bg-background p-4 lg:p-6">
+        <main className="p-4 lg:p-6">
             {children}
-        </div>
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
