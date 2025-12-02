8294175afd3cbbf68bd1fdff3cd88378eed1b465
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-12-02 23:21:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3511014535-25a3b",
+  "appId": "1:423554265650:web:fd8a4a4e0c036bd97e422b",
+  "apiKey": "AIzaSyBVv5zvyhi03spMBeCL2ciXzT5GjC0nwjk",
+  "authDomain": "studio-3511014535-25a3b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "423554265650"
+};
