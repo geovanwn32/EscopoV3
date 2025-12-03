530dee6d292ca577e39db4640524b3d61be0c93f
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-12-03 05:22:16 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogFooter is not d

--- src/app/(app)/admin/page.tsx
+++ src/app/(app)/admin/page.tsx
@@ -19,7 +19,7 @@ import { format, addDays } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Input } from '@/components/ui/input';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from '@/components/ui/alert-dialog';
 
 
 interface User {
