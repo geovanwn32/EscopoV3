ad99bd674aebbe090950639f5552d957ad71a97d
Geovani silva de oliveira <geovanisilvadeoliveira447@gmail.com>
2025-12-07 22:47:46 +0000

    Try fixing this error: `Console TypeError: authorizedDomains is not iter

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
   "projectId": "studio-3511014535-25a3b",
   "appId": "1:423554265650:web:fd8a4a4e0c036bd97e422b",
   "apiKey": "AIzaSyBVv5zvyhi03spMBeCL2ciXzT5GjC0nwjk",
-  "authDomain": "studio-3511014535-25a3b.firebaseapp.com",
+  "authDomain": ["studio-3511014535-25a3b.firebaseapp.com"],
   "measurementId": "",
   "messagingSenderId": "423554265650"
 };
